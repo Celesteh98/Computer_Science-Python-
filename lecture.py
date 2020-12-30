a6cb0a8c8a7f280eeefb8ecb43f655a29df85776
Celeste Herrera <63513076+Celesteh98@users.noreply.github.com>
2020-12-30 10:50:36 -0800

    Add files via upload

--- lecture.py
+++ lecture.py
@@ -1,63 +1,93 @@
-'''
-Binary Search Tree (BST)
-Recall that a binary tree is a tree structure where a node may have at most two children
-    * Binary Search Trees (BST) are binary trees that have the following
-    property:
-    * Values that are less than the parent are found in the left subtree
-    * Values that are greater than the parent are found in the right subtree
-    * This is known as the BST property
-* Binary Search Trees are also one way to implement a Map Abstract Data Type
-    * A Map ADT maps keys to corresponding values
-    * Think of keys defining where in the BST structure a node gets inserted
-And each node has a corresponding value field
-Similar to how Python Dictionaries work on a high-level (but the underlying implementation between a Python Dictionary and BST are different (each with pros / cons))
-Example: Inserting the following keys into a BST
-''''
+''' Binary Search Tree
+* BST Deletion
+* We can break up deletion in three cases:
+    Case 1: When the node to be deleted is a leaf node (no children)
+    Case 2: When the node to be deleted has one child
+    Case 3: When the node to be deleted has two children
 
+Case 1: Case Delete a leaf node
+* Find the node that needs to be deleted
+* Simply remove the parent reference (either left child or right child)
+to the deleted node
+
+Case 2: Delete a node with one child
+* Connect the deleted Node’s parent and the deleted Node’s child together
+
+Case 3: Delete a node with two children
+*First find the successor (node with next greater value) in the right subtree
+    * This can be done by traversing the left children of the node
+    to be deleted’s right subtree
+    * Also note that the successor will always only have at most one child
+        * If the successor had a left child, then it wouldn’t be the successor!
+* Temporarily store the successor and delete the successor from the tree
+    * Deleting the successor will fall in either Case 1 or Case 2
+* Replace the deleted node’s value with the successor’s value
+'''
+
+#BinarySearchTree.py (continuing from last lecture)
 from BinarySearchTree import BinarySearchTree
 
-def test_insertRoot():
-    BST = BinarySearchTree()
-    assert BST.root == None
-    assert BST.length() == 0
-
-def test_insertRoot():
+def test_deleteRoot():
     BST = BinarySearchTree()
     BST.put(10, "ten")
-    assert BST.root.key == 10
-    assert BST.root.payload == "ten"
-    assert BST.root.hasLeftChild() == None
-    assert BST.root.hasRightChild() == None
-    assert BST.root.isLeftChild() == None
-    assert BST.root.isRightChild() == None
-    assert BST.root.isRoot() == True
-    assert BST.root.hasAnyChildren() == None
-    assert BST.root.isLeaf() == True
-    assert BST.root.hasBothChildren() == None
-    assert BST.root.replaceNodeData(20, "twenty" ,None, None)
-    assert BST.root.key == 20
-    assert BST.root.payload == "twenty"
-
-def test_insertNodes():
-    BST = BinarySearchTree()
-    BST.put(10, "ten")
-    BST.put(20, "twenty")
     BST.put(15, "fifteen")
     BST.put(5, "five")
-    assert BST.root.key == 10
-    assert BST.root.leftChild.key == 5
-    assert BST.root.rightChild.key == 20
-    assert BST.root.rightChild.leftChild.key == 15
+    BST.put(3, "three")
+    BST.put(7, "seven")
+    BST.put(12, "twelve")
+    BST.put(17, "seventeen")
+    assert BST.inOrder(BST.root) == \
+           "3 5 7 10 12 15 17"
+    BST.delete(10)
+    assert BST.inOrder(BST.root) == \
+           "3 5 7 12 15 17"
 
-def test_get():
+def test_deleteLeafNode():
     BST = BinarySearchTree()
     BST.put(10, "ten")
-    BST.put(20, "twenty")
     BST.put(15, "fifteen")
     BST.put(5, "five")
-    assert BST.get(10) == "ten"
-    assert BST.get(20) == "twenty"
-    assert BST.get(15) == "fifteen"
-    assert BST.get(5) == "five"
-    assert BST.get(30) == None
-            
+    BST.put(3, "three")
+    BST.put(7, "seven")
+    BST.put(12, "twelve")
+    BST.put(17, "seventeen")
+    BST.delete(3)
+    assert BST.inOrder(BST.root) == \
+        "5 7 10 12 15 17"
+    BST.delete(17)
+    assert BST.inOrder(BST.root) == \
+           "5 7 10 12 15"
+
+def test_deleteNodeWithTwoChildren():
+    BST = BinarySearchTree()
+    BST.put(10, "ten")
+    BST.put(15, "fifteen")
+    BST.put(5, "five")
+    BST.put(3, "three")
+    BST.put(7, "seven")
+    BST.put(12, "twelve")
+    BST.put(17, "seventeen")
+    BST.delete(15)
+    assert BST.inOrder(BST.root) == \
+           "3 5 7 10 12 17"
+    BST.delete(5)
+    assert BST.inOrder(BST.root) == \
+           "3 7 10 12 17"
+        
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
